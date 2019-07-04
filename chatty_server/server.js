98ec87beca47bd710768127630b0a4c2b47e34c7
Cher <red_phoenix417@yahoo.com>
2019-07-04 01:05:24 +0000

    Added websocket server under chatty-server folder

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -10,6 +10,10 @@ const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
+// 
+
+
+
 // Create the WebSockets server
 const wss = new SocketServer({
   server
